b1a1ee626090e583be16509b015e9c96de15ac01
Umbral1013 <Sixela121@hotmail.com>
2021-02-19 13:20:42 -0600

    Edited down figures_HQ to be a little bit cleaner
    I still don't know what i am doing :^).

--- figures_HQ.py
+++ figures_HQ.py
@@ -1,14 +1,11 @@
-""" {{{
+"""
 Programmer's name            : Umbral1013
 Name of the program          : Geometric figures HQ
 Date                         : Between September and December from 2020
                                Revision made on january 20, 2021
+                               Revision made on february 15, 2021
 Description of the program   : Improved version of the first "figures.py"
-------------------------------------
-
-Where:
-x is the magnitude of the side given at the start of the program.
-""" }}}
+"""
 
 from math import pi,tan,sqrt,radians,degrees
 from os import system
@@ -20,176 +17,126 @@ def limpiarEspera():
     sleep(1)
 
 
-def circulo():
+def circulo(circunferencia):
     """
-    Obtain the radius, area and diameter of a circle using only the
-    circumference.
+    Obtener el radio, área y diámetro de un círculo 
+    usando únicamente la circunferencia.
     """
-    circunferencia = float(input("Introduce la circunferencia del círculo\n"))
     radio = circunferencia / (2 * pi)
-    area = (pi) * (radio**2)
+    area = (pi) * (radio ** 2)
     diametro = radio * 2
     limpiarEspera()
-    print("----\nÁrea = %.2f" %area, end=" | ")
-    print("Radio = %.2f" %radio, end=" | ")
-    print("Diámetro = %.2f" %diametro, "\n----")
-    return
+    return radio, area, diametro
 
 
-def trianguloEquilatero():
+def trianguloEquilatero(lado):
     """
-    Obtain the height, perimeter, and area of an equilateral triangle using the
-    value of one side.
+    Obtener la altura, perímetro y área de un triángulo equilátero
+    usando el valor de un lado.
     """
-    valorLado = float(input("Ingresa la medida de un lado\n"))
-    altura = sqrt(valorLado**2 - (1/2 * valorLado)**2)
-    perimetro = valorLado*3
-    area = ((sqrt(3)) / 4) * valorLado**2
+    altura = sqrt(lado ** 2 - (1/2 * lado) ** 2)
+    perimetro = lado * 3
+    area = ((sqrt(3)) / 4) * lado ** 2
     limpiarEspera()
-    print("----\nÁrea = %.2f" %area, end=" | ")
-    print("Perímetro = %.2f" %perimetro, end=" | ")
-    print("Altura = %.2f" %altura, "\n----")
-    return
+    return altura, area, perimetro
 
 
-def cuadrado():
+def cuadrado(lado):
     """
-    Get the area and perimeter of a square using the value of its side.
+    Obtener el área y perímetro de un cuadrado
+    usando la medida de su lado.
     """
-    valorLado = float(input("Introduce la medida de un lado\n"))
     area = valorLado**2
     perimetro = valorLado*4
     limpiarEspera()
-    print("----\nÁrea = %.2f" %area, end=" | ")
-    print("Perímetro = %.2f" %perimetro, "\n----")
-    return
+    return area, perimetro
 
 
-def rectangulo():
+def rectangulo(base, altura):
     """
-    Obtain the sizes of the perimeter and area of a rectangle using the values
-    of its height and base.
+    Obtener el área y perímetro de un rectángulo
+    usando los valores de su altura y base.
     """
-    baseRectangulo = float(input("Introduce la medida de la base\n"))
-    alturaRectangulo = float(input("Introduce la medida de la altura\n"))
-    perimetro = (2*baseRectangulo) + (2*alturaRectangulo)
-    area = baseRectangulo*alturaRectangulo
+    perimetro = (2 * base) + (2 * altura)
+    area = base * altura
     limpiarEspera()
-    print("----\nÁrea = %.2f" %area, end=" | ")
-    print("Perímetro = %.2f" %perimetro, "\n----")
-    return
+    return perimetro, area
 
 
-def rombo():
+def rombo(diagonalMayor, diagonalMenor):
     """
-    Obtain the area and the value of the sides of a rhombus using the
-    magnitudes of its both diagonals.
+    Obtener el área y perímetro de un rombo
+    usando el tamaño de sus dos diagonales.
     """
-    diagonalMayor = float(input("Introduce la medida de la diagonal mayor\n"))
-    diagonalMenor = float(input("Introduce la medida de la diagonal menor\n"))
-    hipotenusa = sqrt(((1/2 * diagonalMayor)**2 + (1/2 * diagonalMenor)**2))
-    area = (diagonalMayor*diagonalMenor)/2
-    perimetro = 4*hipotenusa
+    hipotenusa = sqrt(((1/2 * diagonalMayor) ** 2 + (1/2 * diagonalMenor) ** 2))
+    area = (diagonalMayor * diagonalMenor) / 2
+    perimetro = 4 * hipotenusa
     limpiarEspera()
-    print("----\nÁrea = %.2f" %area, end=" | ")
-    print("Lado = %.2f" %hipotenusa, end=" | ")
-    print("Perímetro = %.2f" %perimetro, "\n----")
-    return
+    return area, perimetro
 
 
-def trapecioIsoceles():
+def trapecioIsoceles(baseMayor, baseMenor, lado):
     """
-    Get the area, perimeter and height of an isosceles trapezoid using the
-    sizes of both bases and the measurement of a side.
+    Conseguir el área, perímetro y altura de un trapecio isóceles
+    usando sus dos bases y la medida de un lado.
     """
-    baseMayor = float(input("Introduce la base mayor\n"))
-    baseMenor = float(input("Introduce la base menor\n"))
-    valorLado = float(input("Introduce la medida de un lado\n"))
-    pitagoras = (baseMayor-baseMenor)/2
-    altura = sqrt(valorLado**2-pitagoras**2)
-    area = ((baseMayor+baseMenor)*altura)/2
-    perimetro = baseMayor+baseMenor+(2*valorLado)
+    pitagoras = (baseMayor - baseMenor) / 2
+    altura = sqrt(lado ** 2 - pitagoras ** 2)
+    area = ((baseMayor + baseMenor) * altura) / 2
+    perimetro = baseMayor + baseMenor + (2 * valorLado)
     limpiarEspera()
-    print("----\nÁrea = %.2f" %area, end=" | ")
-    print("Perímetro = %.2f" %perimetro, end=" ")
-    print("Altura = %.2f" %altura, "\n----")
-    return
+    return altura, area, perimetro
 
 
-def poligono(x):
+def poligono(numeroDeLados, tamañoLado):
     """
-    Get the area, perimeter and apothem using the size of a side.
+    Obtener el área, perímetro y apotema de un polígono regular
+    usando el tamaño de un lado.
     """
-    valorLado = float(input("Introduce la medida de un lado\n"))
     angulo = radians(360 / (2 * x))
-    perimetro = valorLado * x
-    apotema = valorLado / (2 * tan(angulo))
+    perimetro = tamañoLado * numeroDeLados
+    apotema = tamañoLado / (2 * tan(angulo))
     area = (perimetro * apotema) / 2
     limpiarEspera()
-    print("----\nÁrea = %.2f" %area, end=" | ")
-    print("Perímetro = %.2f" %perimetro, end=" | ")
-    print("Apotema = %.2f" %apotema, "\n----")
-    return
+    return perimetro, apotema, area
 
+bienvenida = """
+¡Bienvenido!
 
-def filtroCuadrilatero():
-    """
-    Helps the program to decide which function of the above to use in the case
-    of a quadrilateral.
-    """
-    cuadrilatero = ""
-    while cuadrilatero != 1 and cuadrilatero != 2 and cuadrilatero != 3 and cuadrilatero != 4:
-        cuadrilatero = int(input("1.) Cuadrado\n2.) Rectángulo\n3.) Rombo\n4.) Trapecio isóceles\n"))
-        if cuadrilatero == 1:
-            print("CUADRADO\n----")
-            limpiarEspera()
-            cuadrado()
-        elif cuadrilatero == 2:
-            limpiarEspera()
-            print("RECTÁNGULO\n----")
-            rectangulo()
-        elif cuadrilatero == 3:
-            limpiarEspera()
-            print("ROMBO\n----")
-            rombo()
-        elif cuadrilatero == 4:
-            limpiarEspera()
-            print("TRAPECIO ISÓCELES\n----")
-            trapecioIsoceles()
+Ésta herramienta puede ayudarte a conseguir datos relevantes
+sobre algunas figuras geométricas a partir de como máximo
+tres datos de entrada. Éstos datos varían dependiendo de tu figura
+geométrica.
 
+No están incluidas las figuras geométricas irregulares.
+En el caso de los triángulos, de momento sólamente están soportados
+los triángulos equiláteros.
 
-def filtro(x):
-    """
-    This is where the program decides which function to use if the figure
-    doesn't have 4 sides.
-    """
-    if x <= 0:
-        limpiarEspera()
-        input("Ninguna figura tiene 0 lados.")
-        return
-    elif x in [1,2]:     # "in [1,2]" is more or less the same as "x == 1 and x == 2"
-        limpiarEspera()
-        print("CIRCULO\n----")
-        circulo()
-    elif x == 3:
-        limpiarEspera()
-        print("TRIÁNGULO EQUILÁTERO\n----")
-        trianguloEquilatero()
-    elif x == 4:
-        limpiarEspera()
-        print("CUADRILÁTERO\n----")
-        filtroCuadrilatero()
-    elif x >= 5:
-        limpiarEspera()
-        print("POLÍGONO REGULAR\n----")
-        poligono(lados)
+Es importante que cuando ingreses los datos, únicamente escribas su
+su medida numérica SIN LA UNIDAD DE MEDIDA.
 
+Por ejemplo, ésto está bien:
+    Ingresa la medida de su circunferencia.
+    >>> 3
 
-volverMenu = "s"
+Pero esto no:
+    Ingresa la medida de su circunferencia.
+    >>> 3cm
 
-# This is where the magic happens.
-while volverMenu == "s" or volverMenu == "Si":
-    limpiarEspera()
-    lados = int(input("FIGURAS\n----\nIngresa el número de lados de tu figura\n----\n"))
-    filtro(lados)
-    volverMenu = input("¿Desea volver al menú principal? (s/n)\n")
+Presiona ENTER para continuar...
+"""
+
+limpiarEspera()
+input(bienvenida)
+limpiarEspera()
+print("¿Cuántos lados tiene tu figura?")
+ladosDeMiFigura = int(input())
+
+if ladosDeMiFigura in [1, 2]:
+    print("Ingresa la medida de su circunferencia")
+    circunferencia = int(input())
+    print(circulo(circunferencia))
+elif ladosDeMiFigura == 3:
+    print("Ingresa la medida del lado")
+    ladoTriangulo = int(input())
+    print(trianguloEquilatero(ladoTriangulo))
