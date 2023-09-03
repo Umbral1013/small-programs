7c645930a84f4f2a7c3038f84373288972ab9bf9
Umbral1013 <Sixela121@hotmail.com>
2023-09-03 03:10:18 -0600

    Readability improved

--- 2023-08-17_piramide.c
+++ 2023-08-17_piramide.c
@@ -1,40 +1,41 @@
-/* TÍTULO DEL DOCUMENTO: Pirámide.
+/*
+ * TÍTULO DEL DOCUMENTO: Pirámide.
  * NOMBRE DEL ARCHIVO: piramide.c
  * AUTOR: Umbral1013.
  * PROPÓSITO: Dibujar una 'pirámide' en la pantalla dada la magnitud de la
  * base en caracteres.
  * FECHA: jue 17 ago 2023 22:47:19
-*/
+ */
 
 #include "stdio.h"
-#include "stdlib.h"
 
 int main() {
-    /* i es el contador de 'while'.
-     * b es el número de caracteres que se quiere que ocupe la base.
-     * s es el símbolo que se usará como caracter en la pirámide.
-     */
-    int i, b;
-    char s;
+	/*
+     	 * base es el número de caracteres que se quiere que ocupe la base.
+     	 * simbolo es el caracter que se usará como 'bloque' en la pirámide.
+     	 */
+    	int i, base;
+    	char simbolo;
 
-    i = 0;
-    // s podría ser cualquier otra cosa.
-    s = 'x';
+    	simbolo = 'x';
 
-    printf("PIRAMIDE \n");
-    printf("Cuantos caracteres de longitud deseas que tenga la base? : ");
-    scanf("%d", &b);
-    printf("Que caracter deseas usar? : ");
-    scanf(" %c", &s);
+    	printf("PIRAMIDE\n");
+    	printf("Cuantos caracteres de longitud deseas que tenga la base? : ");
+    	scanf("%d", &base);
+    	printf("Que caracter deseas usar? : ");
+    	scanf(" %c", &simbolo);
 
-    while (i <= b) {
-        /* Esto imprime n caracteres, donde n es el número de línea (la
-         * línea 1 tiene un caracter, la línea dos tiene dos caracteres...
-         */
-        for (int c = 0; c < i; c++) {
-            printf("%c", s);
-        }
-        printf("\n");
-        i++;
-    }
+    	i = 0;
+    	while (i <= base) {
+        	/*
+        	 * Esto imprime n caracteres, donde n es el número de línea (la
+         	 * primera linea tiene un caracter, la línea dos tiene dos caracteres...
+         	 */
+        	for (int c = 0; c < i; c++)
+            		printf("%c", simbolo);
+
+        	printf("\n");
+        	i++;
+    	}
+    	return 0;
 }
